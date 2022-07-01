68446d8611c034480fd4ac63d9b4bbfbf17d34f9
alexmmosac <alexmmosac@outlook.com>
2022-07-01 12:21:52 -0400

    Inital layout with goals for the day.

---
+++ main.spec.js
@@ -0,0 +1,6 @@
+/*
+TODO: Prototype Calculator function. (ADD, SUB, DIV, MUL)
+TODO: Write four basic UNIT test to Accomplish each of the four functions. 
+TODO: Write five Error handing events for Test such as Divide by zero/ being passed a string. 
+*/
+
