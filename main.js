086d5ccf5b8312d8e7c803df63dd7dd754902571
alexmmosac <alexmmosac@outlook.com>
2022-07-01 14:15:33 -0400

    Passes all ADD Function test

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-/*
-TODO: Prototype Calculator function. (ADD, SUB, DIV, MUL)
-TODO: Write four basic UNIT test to Accomplish each of the four functions. 
-TODO: Write five Error handing events for Test such as Divide by zero/ being passed a string. 
-*/
-function add(number_1 ,number_2){
-return null;
+function add(){
+var sum = 0;
+if(arguments !== undefined){
+    if(arguments.length >= 2){              // Adds multiply args together. 
+        for (var i = 0; i < arguments.length; i++) {
+            sum += arguments[i];
+        }
+        return sum;
+    }
+    else if(arguments.length == 0){             // Checks if no args passed.  
+        return Error;
+    }
+    else if(typeof arguments[0] == "object"){           // Checks if array type 
+        var args = Array.prototype.slice.call(arguments[0])     // puts args into new array
+        if(args.length == 0){               //checks for empty array. 
+            return Error;
+        }
+        else{
+        args.forEach(element => {               // adds all args from new array into sum. 
+            sum += element;
+        });
+        return sum;
+    }
+    }
+    else if(typeof arguments[0] == "string"){       // check if string 
+        return Error;
+    }
+    else{
+        return arguments[0];         //return if only passed 1 arg
+    }
+}
+else{
+    return Error;
+
+}
 }
 
 function sub(number_1, number_2){
--- main.spec.js
+++ main.spec.js
@@ -23,19 +23,19 @@ describe('Basic Calculator test', () => {
 });
 
 // ADD - ERROR Handling/ TYPE handling.
-describe('Basic Calculator test', () => {
+describe('Error Handling test', () => {
     test('Handles strings if passed', () => {
       expect(main.add("Hello Error!")).toEqual(Error); // IF passed "Hello Error" function will return a Error
     });
 });
-describe('Basic Calculator test', () => {
+describe('Error Handling test', () => {
     test('Handles Empty arrays', () => {
         var array = [];
       expect(main.add(array)).toEqual(Error); // If passed array containing nothing this function will return an Error
     });
 });
 
-describe('Basic Calculator test', () => {
+describe('Error Handling test', () => {
     test('Handles Empty input', () => {
       expect(main.add()).toEqual(Error); // If nothing is passed this function will return an Error!
     });
@@ -43,83 +43,83 @@ describe('Basic Calculator test', () => {
 
 // SUBs -  Basic Functionality
 describe('Basic Calculator test', () => {
-    test('subs two numbers', () => {
+    test.skip('subs two numbers', () => {
       expect(main.sub(4,2)).toEqual(2);   // if passed 4 and 2 this funciton will return 2
     });
 });
 describe('Basic Calculator test', () => {
-    test('subs Multiple numbers', () => {
+    test.skip('subs Multiple numbers', () => {
       expect(main.sub(100, 50, 40)).toEqual(10); // if passed 100, 50, and 40 this function will return 10
     });
 });
 describe('Basic Calculator test', () => {
-    test('subs an array of  numbers', () => {
+    test.skip('subs an array of  numbers', () => {
         var array = [100, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
       expect(main.sub(array)).toEqual(45);  //if passed array containg 100 and all numbers of 1-10 this function will return 45
     });
 });
 // SUBs - ERROR Handling
-describe('Basic Calculator test', () => {
-    test('Handles strings if passed', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles strings if passed', () => {
       expect(main.sub("Goodbye Error!")).toEqual(Error);  // IF passed "Hello Error" function will return a Error
     });
 });
 
-describe('Basic Calculator test', () => {
-    test('Handles Empty arrays if passed', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles Empty arrays if passed', () => {
         var array = [];
       expect(main.sub(array)).toEqual(Error);     // If passed array containing nothing this function will return an Error
     });
 });
 
-describe('Basic Calculator test', () => {
-    test('Handles Empty input', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles Empty input', () => {
       expect(main.sub()).toEqual(Error);        // If nothing is passed this function will return an Error!
     });
 });
 // DIVs - Basic Functionality
 describe('Basic Calculator test', () => {
-    test('div two numbers', () => {
+    test.skip('div two numbers', () => {
       expect(main.div(50, 2)).toEqual(25);    // if 50 and 2 are passed this function will return 25!
     });
 });
 
 describe('Basic Calculator test', () => {
-    test('divs Multiple numbers', () => {
+    test.skip('divs Multiple numbers', () => {
       expect(main.div(100, 25, 2)).toEqual(2); // if passed 100, 25 and 2 this function will return 2
     });
 });
 describe('Basic Calculator test', () => {
-    test('divs an array of  numbers', () => {
+    test.skip('divs an array of  numbers', () => {
     var array = [100, 5, 4, 3, 2, 1];
       expect(main.div(array)).toEqual(0.83);    // if passed array containing 100, 5, 4, 3, 2, and 1 it will return 0.833333
     });
 });
 
 //DIVs -  Basic ERROR Handling
-describe('Basic Calculator test', () => {
-    test('Handles strings if passed', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles strings if passed', () => {
       expect(main.div("Welcome Error!")).toEqual(Error); // if passed a string it will return an error 
     });
 });
 
-describe('Basic Calculator test', () => {
-    test('Handles Empty arrays if passed', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles Empty arrays if passed', () => {
         var array = [];
       expect(main.div(array)).toEqual(Error);   // if passed an array it will return an error
     });
 });
 
-describe('Basic Calculator test', () => {
-    test('Handles Empty input', () => {
+describe('Error Handling  test', () => {
+    test.skip('Handles Empty input', () => {
       expect(main.div()).toEqual(Error);        //if passed nothing it will return an error
     });
 });
 
 //DIVs - Divde by zero ERROR 
 
-describe('Basic Calculator test', () => {
-    test('Divide by zero', () => {
+describe('Error Handling test', () => {
+    test.skip('Divide by zero', () => {
       expect(main.div(0, 0)).toEqual(Error);        /*** SPECIAL CASE: if passed zero and zero it will return Error as you can't divide by zero.*/
     });
 });
@@ -127,38 +127,38 @@ describe('Basic Calculator test', () => {
 
 // MULs - Basic Functionality 
 describe('Basic Calculator test', () => {
-    test('muls two numbers', () => {
+    test.skip('muls two numbers', () => {
       expect(main.mul(2,3)).toEqual(6);     //if passed 2 and 3 it will return 6
     });
 });
 describe('Basic Calculator test', () => {
-    test('muls Multiple numbers', () => {
+    test.skip('muls Multiple numbers', () => {
       expect(main.mul(2,3,2)).toEqual(12);  // if passed 2, 3, and 2 it will return 12 
     });
 });
 describe('Basic Calculator test', () => {
-    test('muls an array of  numbers', () => {
+    test.skip('muls an array of  numbers', () => {
         var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
       expect(main.mul(array)).toEqual(3628800);  // if passed array with 1 through 10 it will return 3,628,800
     });
 });
 
 //MULs -  Basic ERROR Handling
-describe('Basic Calculator test', () => {
-    test('Handles strings if passed', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles strings if passed', () => {
       expect(main.mul("Your Almost done!")).toEqual(Error); // if passed a string it will return an error!
     });
 });
 
-describe('Basic Calculator test', () => {
-    test('Handles Empty arrays if passed', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles Empty arrays if passed', () => {
         var array = [];
       expect(main.mul(array)).toEqual(Error);  // if passed an empty array it will return an error
     });
 });
 
-describe('Basic Calculator test', () => {
-    test('Handles Empty input', () => {
+describe('Error Handling test', () => {
+    test.skip('Handles Empty input', () => {
       expect(main.mul()).toEqual(Error);  // if passed nothing it will return an Error!
     });
 });
